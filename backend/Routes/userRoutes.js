492e3f1623bfd5666f0095e674fb8bb06be8f89d
Tejash <97391977+tejashsingh@users.noreply.github.com>
2023-08-03 14:13:55 +0530

    Updated search user

--- backend/Routes/userRoutes.js
+++ backend/Routes/userRoutes.js
@@ -1,10 +1,10 @@
 const express = require("express");
 const { allUsers, registerUser, authUser } = require("../Controllers/userControllers");
-
+const {protect}= require("../middleware/authMiddleware");
 const router = express.Router();
 
 // router.route("/").get(protect, allUsers);
-router.route("/").post(registerUser).get(allUsers);
+router.route("/").post(registerUser).get(protect, allUsers);
 router.post("/login", authUser);
 
 module.exports = router;
